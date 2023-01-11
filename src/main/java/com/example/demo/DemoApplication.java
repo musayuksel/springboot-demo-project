da6a5cc079010775b59edbe808f676d7e2bfded1
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-11 16:35:58 +0000

    Add andis end point

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.andi.Andi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
@@ -16,8 +19,27 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public  String hello(){
-		return "Hello world";
+	@GetMapping("/andis")
+	public List<Andi> andis(){
+		return List.of(
+				new Andi(
+						1,
+						"Musa",
+						"Yuksel",
+						"Jemison",
+						1.1,
+						"PD"
+
+				),
+				new Andi(
+						2,
+						"Musa2",
+						"Yuksel2",
+						"Jemison",
+						1.1,
+						"PD"
+
+				)
+		);
 	}
 }
