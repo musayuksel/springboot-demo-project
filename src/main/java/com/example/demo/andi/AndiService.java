108d64fcd049516a9b8e83fc09ea3c55c86dceee
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-11 21:55:27 +0000

    Add factory

--- src/main/java/com/example/demo/andi/AndiService.java
+++ src/main/java/com/example/demo/andi/AndiService.java
@@ -1,32 +1,31 @@
 package com.example.demo.andi;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 //@Component . more general
 @Service
 public class AndiService {
+    private final AndiRepository andiRepository;
+    @Autowired
+    public AndiService(AndiRepository andiRepository) {
+        this.andiRepository = andiRepository;
+    }
+
+
     public List<Andi> getAndis(){
-        return List.of(
-                new Andi(
-                        1,
-                        "Musa",
-                        "Yuxel",
-                        "Jemison",
-                        1.1,
-                        "PD"
-
-                ),
-                new Andi(
-                        2,
-                        "Musa2",
-                        "Yuxel",
-                        "Jemison",
-                        1.2,
-                        "PD"
-
-                )
-        );
+        return andiRepository.findAll();
+//        return List.of(
+//                new Andi(
+//                        1,
+//                        "Musa",
+//                        "Yuxel",
+//                        "Jemison",
+//                        1.1,
+//                        "PD"
+//
+//                )
+//        );
     }
 }
