b197b1372557b2427a2651fa02e3055f19903c72
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-12 10:10:41 +0000

    Add Query to the interface

--- src/main/java/com/example/demo/andi/AndiRepository.java
+++ src/main/java/com/example/demo/andi/AndiRepository.java
@@ -1,9 +1,15 @@
 package com.example.demo.andi;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AndiRepository
         extends JpaRepository<Andi,Integer> {
+
+//    @Query("SELECT a FROM Andi andi  WHERE andi.email = ?1")//we can use this way for complicated queries
+    Optional<Andi> findAndiByEmail(String email);//    SELECT * FROM andisdemoproject WHERE email =?
 }
