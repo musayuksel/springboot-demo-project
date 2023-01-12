0cdd498cd3f6646307d1f982ee77cd5f3516d11c
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-12 13:13:26 +0000

    Add setter for Andi

--- src/main/java/com/example/demo/andi/Andi.java
+++ src/main/java/com/example/demo/andi/Andi.java
@@ -81,4 +81,28 @@ public class Andi {
     public String getRole() {
         return role;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSquad(String squad) {
+        this.squad = squad;
+    }
+
+    public void setLevel(double level) {
+        this.level = level;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
