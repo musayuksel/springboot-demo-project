a381b133ca06390dde07ab2b34991e934058adba
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-16 16:57:42 +0000

    Add test should Throw Error When Add Andi With Duplicate Email

--- src/test/java/com/example/demo/andi/AndiServiceTest.java
+++ src/test/java/com/example/demo/andi/AndiServiceTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -33,6 +34,19 @@ class AndiServiceTest {
         andiService.addNewAndi(andiToSave);
         verify(andiRepository, times(1)).findAndiByEmail("unique_email@example.com");
         verify(andiRepository, times(1)).save(andiToSave);
+        verifyNoMoreInteractions(andiRepository);
+    }
+
+    @Test
+    void shouldThrowErrorWhenAddAndiWithDuplicateEmail() {
+        Andi andiToSave = new Andi("duplicate_email@example.com","Musa", "Yxel", "Endeavour", 1.3, "PD");
+
+        // setup mock to return non-empty optional when searching for duplicate email
+        Andi existingAndi = new Andi("duplicate_email@example.com","Musa22", "Yxel", "Endeavour", 1.3, "PD");
+        when(andiRepository.findAndiByEmail("duplicate_email@example.com")).thenReturn(Optional.of(existingAndi));
+
+        assertThrows(IllegalStateException.class, () -> andiService.addNewAndi(andiToSave));
+        verify(andiRepository, times(1)).findAndiByEmail("duplicate_email@example.com");
         verifyNoMoreInteractions(andiRepository);
     }
 
